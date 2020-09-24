7ff06e260853411fbd339fb79d899fb779831056
Chenzs108 <chenzs108@outlook.com>
2020-09-24 11:18:31 +0800

    Feat (Server): Make RAT itself a module

--- server/internal/rat/rat.go
+++ server/internal/rat/rat.go
@@ -8,6 +8,7 @@ import (
 	"net"
 	"sync"
 
+	"server/internal/mod"
 	"server/internal/network"
 	"server/internal/utility"
 )
@@ -22,6 +23,9 @@ const (
 // RAT is the interface definition of the remote administration tool.
 type RAT interface {
 	io.Closer
+
+	// Register registers a module.
+	Register(mod mod.Module) error
 
 	// Startup starts the remote administration tool.
 	Startup(port int) error
@@ -39,6 +43,10 @@ type rat struct {
 	abort chan bool
 	wg    sync.WaitGroup
 
+	mod.Dispatcher
+	cmdHandlers mod.CmdHandlerMap
+	netHandlers mod.NetHandlerMap
+
 	utility.LogQue
 }
 
@@ -46,9 +54,20 @@ type rat struct {
 func NewRAT(logger utility.LogQue) RAT {
 	utility.Assert(logger != nil, "Null logger.")
 
-	return &rat{
-		LogQue: logger,
+	rat := &rat{
+		LogQue:      logger,
+		Dispatcher:  mod.NewDispatcher(),
+		cmdHandlers: make(mod.CmdHandlerMap),
+		netHandlers: make(mod.NetHandlerMap),
 	}
+
+	// Add command handlers and packet handlers.
+
+	if err := rat.Register(rat); err != nil {
+		logger.Panic(err)
+	}
+
+	return rat
 }
 
 func (rat *rat) Startup(port int) error {
@@ -69,7 +88,8 @@ func (rat *rat) Startup(port int) error {
 func (rat *rat) Exec(cmd string, args []string) error {
 	rat.LogStorage()
 	if rat.currClient != nil && rat.sockets.Get(rat.currClient.ID()) == nil {
-		rat.currClient = nil
+		rat.Log(fmt.Sprintf("The current client [%v] has become invalid.", rat.currClient.ID()))
+		rat.SetClient(nil)
 	}
 
 	if cmd == "" {
@@ -78,16 +98,65 @@ func (rat *rat) Exec(cmd string, args []string) error {
 
 	defer rat.LogStorage()
 
-	return nil
+	mod := rat.ByCmd(cmd)
+	if mod == nil {
+		return fmt.Errorf("The command is invalid: %v", cmd)
+	}
+
+	// Give the command to the sub-module.
+	if mod != rat {
+		return mod.Exec(cmd, args)
+	}
+
+	// Handle commands supported by RAT itself.
+	handler, ok := rat.cmdHandlers[cmd]
+	utility.Assert(ok, "The module has registered an invalid command.")
+
+	return handler(args)
 }
 
-// Respond handles packets.
+func (rat *rat) Cmds() []string {
+	cmds := make([]string, 0, len(rat.cmdHandlers))
+	for c := range rat.cmdHandlers {
+		cmds = append(cmds, c)
+	}
+
+	return cmds
+}
+
+// Respond only handles packets supported by RAT itself.
 func (rat *rat) Respond(client network.Client, packet *network.Packet) error {
-	return nil
+	handler, ok := rat.netHandlers[packet.Type]
+	utility.Assert(ok, "The module has registered an invalid packet type.")
+
+	return handler(client, packet)
+}
+
+func (rat *rat) Packets() []network.PacketType {
+	types := make([]network.PacketType, 0, len(rat.netHandlers))
+	for t := range rat.netHandlers {
+		types = append(types, t)
+	}
+
+	return types
+}
+
+func (rat *rat) ID() mod.ModuleID {
+	return 0
+}
+
+func (rat *rat) Name() string {
+	return "RAT"
 }
 
 func (rat *rat) SetClient(client network.Client) {
 	rat.currClient = client
+
+	for _, mod := range rat.All() {
+		if mod != rat {
+			mod.SetClient(client)
+		}
+	}
 }
 
 // Close terminates the remote administration tool.
@@ -153,7 +222,15 @@ func (rat *rat) transfer(socket *socket) {
 			return
 		}
 
-		err = rat.Respond(client, packet)
+		mod := rat.ByPacket(packet.Type)
+		if mod == nil {
+			rat.Store(
+				fmt.Errorf("The client [%v] has received a packet with invalid type: %v",
+					client.ID(), packet.Type))
+			continue
+		}
+
+		err = mod.Respond(client, packet)
 		if err != nil {
 			if errors.Is(err, io.EOF) {
 				rat.sockets.Del(client.ID())
