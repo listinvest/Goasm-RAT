e6da5dc9f95289c138d1f856d3ac285a92b75498
Chenzs108 <chenzs108@outlook.com>
2020-10-14 00:17:09 +0800

    Chore (Client): Add a 'bin' directory to store the executable file

--- client/build.ps1
+++ client/build.ps1
@@ -1,7 +1,10 @@
+New-Variable -Name 'AppName' -Value 'client' -Option Constant
+
+New-Item -Name 'bin' -ItemType 'Directory' -Force
 New-Item -Name 'build' -ItemType 'Directory' -Force
 Set-Location -Path '.\build'
 
 Start-Process -FilePath 'ml' -ArgumentList '/c', '/coff', '../src/*.asm' -NoNewWindow -Wait
-Start-Process -FilePath 'link' -ArgumentList '/subsystem:console', '/out:client.exe', '*.obj' -NoNewWindow -Wait
+Start-Process -FilePath 'link' -ArgumentList '/subsystem:console', "/out:$AppName.exe", '*.obj' -NoNewWindow -Wait
 
-Remove-Item '.\*.obj'
+Move-Item -Path ".\$AppName.exe" -Destination '..\bin' -Force
